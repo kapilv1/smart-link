ebafefaf293dc2c42537d70273649a20bab030b2
gabrielcleverwolf623 <gabriel.cleven~olf623@gmail.com>
2026-06-07 03:40:37 -0400

    fontend connasdfaected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "https://smart-link-henna.vercel.app";
+const API_URL = "https://smart-link-915g7xyxg-kapilv1s-projects.vercel.app";
 
 function getType(url) {
   const lowerUrl = url.toLowerCase();
