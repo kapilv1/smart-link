7b85af9135b948e1cc126ff143f1859765790fff
gabrielcleverwolf623 <gabriel.cleven~olf623@gmail.com>
2026-06-07 03:39:01 -0400

    fontend connected and server also

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://smart-link-henna.vercel.app";
 
 function getType(url) {
   const lowerUrl = url.toLowerCase();
